1f61209948355d6ec571fe644a63856e16dc9c64
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-07-09 12:46:38 +0200

    create result screen

---
+++ src/components/Result.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Result.css';
+
+const Result = () => {
+    return ( 
+        <div className="result-container">
+            <h2>69,69</h2>
+        </div>
+     );
+}
+ 
+export default Result;
